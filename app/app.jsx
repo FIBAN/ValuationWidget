26c807f2008edddcbf9e956b026bc324af7eafca
Hannu Huhtanen <hannu.huhtanen@gmail.com>
2017-02-14 15:36:34 +0200

    removed foundation

--- app/app.jsx
+++ app/app.jsx
@@ -3,15 +3,10 @@ import {render} from 'react-dom';
 import ValuationWidget from './components/ValuationWidget';
 
 // Load foundation
-require('style-loader!css-loader!foundation-sites/dist/css/foundation.min.css');
-$(document).foundation();
+//require('style-loader!css-loader!foundation-sites/dist/css/foundation.min.css');
+//$(document).foundation();
 
 // Load App css
 require('style-loader!css-loader!sass-loader!./styles/app.scss');
 
-render(
-  <div className="row">
-      <ValuationWidget/>
-  </div>,
-  document.getElementById('valuationWidget')
-);
+render(<ValuationWidget/>, document.getElementById('valuationWidget'));
