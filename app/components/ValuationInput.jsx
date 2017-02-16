4dab62fc831d2f1877c9e048a7268fed9282b11a
Hannu Huhtanen <hannu.huhtanen@gmail.com>
2017-02-16 12:07:39 +0200

    increased responsivity

--- app/components/ValuationInput.jsx
+++ app/components/ValuationInput.jsx
@@ -17,13 +17,13 @@ class ValuationInput extends React.Component {
     );
   }
 
-  inputRow(id, label, value, unit, ref) {
+  inputRow(id, label, value, unit, ref, isDisabled) {
     return (
       <tr className="inputRow">
         <td><label htmlFor={id}>{label + ":"}</label></td>
         <td>
           <div className="inputCol">
-            <input id={id} step="any" value={value} onChange={this.handleChange} ref={ref} /><span>{unit}</span>
+            <input id={id} step="any" value={value} onChange={this.handleChange} ref={ref} disabled={isDisabled ? "disabled": null}/><span>{unit}</span>
           </div>
         </td>
       </tr>
@@ -37,11 +37,11 @@ class ValuationInput extends React.Component {
         <form onSubmit={this.handleSubmit}>
           <table>
             <tbody>
-            {this.inputRow("reqReturn",     "Required return",             this.props.reqReturn,   "%",   ((i) => this.reqReturn = i))}
-            {this.inputRow("entrySales",    "Entry sales",             this.props.entrySales,"€",  ((i) => this.entrySales = i))}
-            {this.inputRow("salesMargin",   "Sales margin of company", this.props.salesMargin,"%", ((i) => this.salesMargin = i))}
-            {this.inputRow("invPeriod",     "Investment period",   this.props.invPeriod,"years",   ((i) => this.invPeriod = i))}
-            {this.inputRow("initInvest",    "Initial investment",      this.props.initInvest,"€",  ((i) => this.initInvest = i))}
+            {this.inputRow("reqReturn",     "Required return",         this.props.reqReturn,   "%",     ((i) => this.reqReturn = i), true)}
+            {this.inputRow("entrySales",    "Entry sales",             this.props.entrySales,  "€",     ((i) => this.entrySales = i))}
+            {this.inputRow("salesMargin",   "Sales margin of company", this.props.salesMargin, "%",     ((i) => this.salesMargin = i))}
+            {this.inputRow("invPeriod",     "Investment period",       this.props.invPeriod,   "years", ((i) => this.invPeriod = i))}
+            {this.inputRow("initInvest",    "Initial investment",      this.props.initInvest,  "€",     ((i) => this.initInvest = i))}
             </tbody>
           </table>
          </form>
