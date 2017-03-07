33073e7ceef24687b0f325ed4e284a5a145ebabb
Hannu Huhtanen <hannu.huhtanen@gmail.com>
2017-03-07 16:29:06 +0200

    changed slider colors

--- app/app.jsx
+++ app/app.jsx
@@ -2,13 +2,10 @@ import React from 'react';
 import {render} from 'react-dom';
 import ValuationWidget from './components/ValuationWidget';
 
-// Load foundation
-//require('style-loader!css-loader!foundation-sites/dist/css/foundation.min.css');
-//$(document).foundation();
+// Load react slider css
+require('style-loader!css-loader!../node_modules/rc-slider/assets/index.css');
 
 // Load App css
 require('style-loader!css-loader!sass-loader!./styles/app.scss');
-
-require('style-loader!css-loader!../node_modules/rc-slider/assets/index.css');
 
 render(<ValuationWidget/>, document.getElementById('valuationWidget'));
